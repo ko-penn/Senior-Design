b5e1b842ce714102a0753d5425f1d0749c4a6c90
Nicholas <ferko410us@gmail.com>
2023-11-22 11:17:55 -0500

    Style change to input and buttons

--- Application/components/Login.jsx
+++ Application/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
 
 export default function Login({childToParent}) {
 
@@ -13,7 +13,7 @@ export default function Login({childToParent}) {
           style={styles.input}
           onChangeText={onChangeLogin}
           value={login}
-          placeholder="Login"
+          placeholder="Username"
         />
         <TextInput
           style={styles.input}
@@ -21,10 +21,12 @@ export default function Login({childToParent}) {
           value={password}
           placeholder="Password"
         />
-        <Button onPress = {() => childToParent("start")}
-          title="Login"
-          color="#841584"
-          accessibilityLabel="Logging you in" />
+        <TouchableOpacity onPress={() => childToParent("start")}
+        style={styles.loginButton}
+          accessibilityLabel="Logging you in"
+        >
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
         <Text style={styles.createtext} onPress={() => childToParent("accountpress")}>
           Create new account
         </Text>
@@ -43,11 +45,25 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 10,
-    width: 200,
+    width: 300,
     borderWidth: 1,
+    borderRadius:10,
     padding: 5,
   },
   createtext: {
     textDecorationLine: 'underline',
   },
+  loginText: {
+    color:'#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 16
+  },
+  loginButton: {
+    backgroundColor:'#841584', 
+    padding:10, 
+    borderRadius:20,
+    marginBottom:30,
+    width:300
+  }
 });
--- Application/components/Start.jsx
+++ Application/components/Start.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput, Image } from 'react-native';
 
 export default function Start({childToParent}) {
 
@@ -7,6 +7,7 @@ export default function Start({childToParent}) {
 
   return (
     <View style={styles.container}>
+      <Image source={require('../assets/Mystery-Man.webp')} style={styles.mysteryMan}/>
       <Text>Enter a description of yourself:</Text>
       <TextInput
         style={styles.input}
@@ -22,10 +23,12 @@ export default function Start({childToParent}) {
         value={description}
         placeholder=""
       />
-      <Button onPress = {() => childToParent("matchmaking")}
-        title="Start Matchmaking"
-        color="#841584"
-        accessibilityLabel="Starting matchmanking" />
+        <TouchableOpacity onPress={() => childToParent("start")}
+        style={styles.matchmakeButton}
+          accessibilityLabel="Starting matchmanking"
+        >
+          <Text style={styles.matchmakeText}>Start Matchmaking</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -39,9 +42,27 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    width: 200,
-    margin: 12,
+    margin: 10,
+    width: 300,
     borderWidth: 1,
-    padding: 10,
+    borderRadius:10,
+    padding: 5,
   },
+  mysteryMan: {
+    width: 200,
+    height:200,
+  },
+  matchmakeText: {
+    color:'#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 16
+  },
+  matchmakeButton: {
+    backgroundColor:'#841584', 
+    padding:10, 
+    borderRadius:20,
+    marginBottom:30,
+    width:300
+  }
 });
