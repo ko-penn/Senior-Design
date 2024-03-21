02b6618d47e01742f3fae6d285be54e62b55cf2f
Kody <pennky@mail.uc.edu>
2024-03-20 21:01:13 -0400

    Switched to bottom navbar, added separate settings camera, disabled popup warnings

--- Application/App.jsx
+++ Application/App.jsx
@@ -7,11 +7,13 @@ import Start from './components/Start';
 import Create from './components/Create';
 import Settings from './components/Settings';
 import Profile from './components/Profile';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer, useNavigationContainerRef, } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import Status from './components/Status';
-import { Account, AccountContext } from "./components/Account"
-import MyCamera from './components/MyCamera';
+import { Account, AccountContext } from "./components/Account";
+import SettingsCamera from './components/SettingsCamera';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { LogBox } from 'react-native';
 
 function HomeScreen({ navigation }) {
   return (
@@ -35,9 +37,9 @@ function SettingsScreen({ navigation }) {
   );
 }
 
-function SettingsCamera({ navigation }) {
+function SettingsCameraScreen({ navigation }) {
   return (
-    <MyCamera></MyCamera>
+    <SettingsCamera></SettingsCamera>
   );
 }
 
@@ -47,16 +49,12 @@ function Logout({ childToParent }) {
     childToParent("login");
   }, []);
   return(null);
-  /*return (
-    <div style={styles.container}>
-      <Text style={styles.logoutText}>Logging you out. Please wait.</Text>
-    </div>
-  );*/
 }
 
 export default function App() {
-  const Drawer = createDrawerNavigator();
+  const Tab = createBottomTabNavigator();
   const [data, setData] = useState('login');
+  LogBox.ignoreAllLogs(); //Disables popup warnings on device
 
   const childToParent = (childdata) => {
     setData(childdata);
@@ -102,17 +100,45 @@ export default function App() {
       <>
       <Account>
         <NavigationContainer name='nav'>
-          <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={HomeScreen}/>
-            <Drawer.Screen name="Profile" component={ProfileScreen}/>
-            <Drawer.Screen name="Settings" component={SettingsScreen}/>
-            <Drawer.Screen name="MyCamera" component={SettingsCamera}/>
-            <Drawer.Screen name="Logout" component={() => <Logout childToParent={childToParent}/>}/>
-          </Drawer.Navigator>
+          <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === 'Home') {
+                  iconName = 'home-outline';
+                } 
+                else if (route.name === 'Profile') {
+                  iconName = 'person-circle-outline';
+                }
+                else if (route.name === 'Settings') {
+                  iconName = 'settings-outline';
+                }
+                else if (route.name === 'Camera') {
+                  iconName = 'camera-outline';
+                }
+                else if (route.name === 'Logout') {
+                  iconName = 'exit-outline';
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: '#31a9ce',
+              tabBarInactiveTintColor: 'gray',
+          })}>
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Profile" component={ProfileScreen}/>
+            <Tab.Screen name="Settings" component={SettingsScreen}/>
+            <Tab.Screen name="Camera" component={SettingsCameraScreen}/>
+            <Tab.Screen name="Logout" component={() => <Logout childToParent={childToParent}/>}/>
+          </Tab.Navigator>
         </NavigationContainer>
         <Status /></Account>
       </>
     );
+
   }
   else{
     return (
---
+++ Application/components/SettingsCamera.jsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, SafeAreaView, Image, Platform } from 'react-native';
+import { useState, useEffect, useRef, useContext } from 'react';
+/*https://www.youtube.com/watch?v=4WPjWK0MYMI*/
+import { Camera } from 'expo-camera';
+import { useIsFocused } from '@react-navigation/native';
+import Session from './Session';
+import { uploadToS3 } from './s3Upload';
+import Pool from "./UserPool";
+
+
+export default function MyCamera() {
+  let cameraRef = useRef();
+  const [hasCameraPermission, sethasCameraPermission] = useState();
+  const [photo, setPhoto] = useState();
+  const [sess, setSess] = useState('false');
+  const isFocused = useIsFocused();
+  const plat = Platform.OS;
+
+
+  const onUse = (event) => {
+    setSess("true");
+    const user= Pool.getCurrentUser().getUsername();
+    const base64Photo = photo.base64.replace(/^data:image\/\w+;base64,/, "");
+    const {error, key} = uploadToS3(base64Photo, user);
+    if(error){
+      console.debug("onFailure: ", err);
+      reject(err);
+    }
+    else{
+      console.debug("success", key)
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      sethasCameraPermission(cameraPermission.status === "granted");
+    })();
+  }, []);
+
+  if(hasCameraPermission === undefined){
+    return <Text>Requesting permissions...</Text>
+  }
+  else if (!hasCameraPermission){
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>
+  }
+  else if (hasCameraPermission){
+    let takePic = async () => {
+        let options = {
+          quality: 1,
+          base64: true,
+          exif: false
+        };
+    
+        let newPhoto = await cameraRef.current.takePictureAsync(options);
+        setPhoto(newPhoto);
+    };
+
+    if (photo && sess==='false') {
+      if (plat === 'ios' || plat === 'android'){
+        return (
+          <SafeAreaView style={styles.container}>
+            <Image style={styles.preview} source={photo} />
+            <Button title="Use" onPress={onUse}/>
+            <Button title="Discard" onPress={() => setPhoto(undefined)} />
+          </SafeAreaView>
+        );
+      }
+      else{
+        return (
+          <SafeAreaView style={styles.container}>
+            <Image style={styles.comppreview} source={photo} />
+            <Button title="Use" onPress={onUse}/>
+            <Button title="Discard" onPress={() => setPhoto(undefined)} />
+          </SafeAreaView>
+        );
+      }
+    }
+    else if (photo && sess) {
+      return (
+          <Session></Session>
+        );
+  }
+    else if (photo === undefined && isFocused){
+      if (plat === 'ios' || plat === 'android'){
+        return(
+          <Camera style={styles.camera} ref={cameraRef}>
+            <View style={styles.buttonContainer}>
+              <Button title="Take Pic" onPress={takePic} />
+            </View>
+          </Camera>
+        )
+      }
+      else{
+        return(
+          <Camera style={styles.compcamera} ref={cameraRef}>
+            <View style={styles.buttonContainer}>
+              <Button title="Take Pic" onPress={takePic} />
+            </View>
+          </Camera>
+        )
+      }
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  camera: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+  },
+  compcamera: {
+    width: '50%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+  },
+  buttonContainer: {
+    backgroundColor: '#fff',
+  },
+  preview: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
+  comppreview: {
+    flex: 1,
+    resizeMode: 'contain',
+    transform: [{scaleX: -1}],
+  },
+  imageContainer: {
+    flex: 1,
+    objectFit: 'contain'
+  }
+});
